0724af16ba4232308e5584bf1ae91e0637959535
ItsMeShane <pumpkinbrainhead@gmail.com>
2023-12-10 23:29:22 -0400

    initial commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+<Section row nopadding>
+    <LeftSection>
+        <SectionTitle main center>
+            Welcome to <br/>
+            My Personal Portfolio
+        </SectionTitle>
+        <SectionText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </SectionText>
+        <Button onClick={() => window.location = "https://firefox.com"}> Learn More</Button>
+    </LeftSection>
+</Section>
+);
+
+export default Hero;
